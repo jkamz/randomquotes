97972d94883b807a93840534d0c2fc682b1e11ed
jkamz <jkamz6432@gmail.com>
2017-06-27 21:58:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$ (document).ready(function(){
+	function getNewQuote (){
+		var quote;
+		var author;
+
+		$.ajax({
+			url: 'http://api.forismatic.com/api/1.0/',
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp',
+			},
+			success: function(response){
+				quote = response.quoteText;
+				author = response.quoteAuthor;
+				$('#quote').text(quote);
+
+				if (author) {
+					$("#author").text('- ' + author);
+				} else {
+					$("#author").text('-unknown');
+				}
+			}
+
+		});
+
+	}
+	//getNewQuote();
+
+	$("button").click(function(){
+			getNewQuote();
+		});
+});
